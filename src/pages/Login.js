f50ed84ec1802a69b93c71d42d8e5f3f1794db83
Kainy Ryu <kainy2109@gmail.com>
2020-11-14 19:12:26 +0900

    :triangular_flag_on_post: Create Login page
    Create Login page with StyledFirebaseAuth

---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import logo from '../assets/logo.png';
+import fInstance, { firebaseAuthService } from '../fbase';
+
+export default function Login() {
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      fInstance.auth.GoogleAuthProvider.PROVIDER_ID,
+      fInstance.auth.FacebookAuthProvider.PROVIDER_ID,
+      fInstance.auth.TwitterAuthProvider.PROVIDER_ID,
+      fInstance.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <div>
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebaseAuthService}
+        />
+      </div>
+    </div>
+  );
+}
+
