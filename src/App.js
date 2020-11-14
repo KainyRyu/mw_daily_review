0c36d039e36e335ba12cb7acd96ca393975aecac
Kainy Ryu <kainy2109@gmail.com>
2020-11-15 01:35:31 +0900

    :lipstick: Create Header component
    Link to "/" on the date and link to "/priority" on the list icon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
           <Route path="/login" component={Login} />
         </Switch>
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import listIcon from '@iconify/icons-bi/list-check';
+import Icon from '@iconify/react';
+import styled from 'styled-components';
+
+export default function Header() {
+
+  function today() {
+    let newDate = new Date();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satuday'];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let day = days[newDate.getDay()];
+    let date = newDate.getDate();
+    let month = months[newDate.getMonth()];
+    let year = newDate.getFullYear();
+    return `${day}, ${date} ${month} ${year}`
+  }
+
+  return (
+    <TopBar>
+      <Link to="/"><h2>{today()}</h2></Link>
+      <Link to="/priority">
+        <Icon icon={listIcon} style={{fontSize: '37px'}} />
+      </Link>
+    </TopBar>
+  )
+}
+
+const TopBar = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 20px;
+  border-bottom: #222222 solid 2px;
+`
