a7d7def624d28291f69942561316127da07dfdf3
KainyRyu <kainy2109@gmail.com>
2020-11-22 15:51:54 +0900

    :lipstick: Add styling(Icon, Legal Pad design)
    install inconify, Change the design colour and style as Legal Pad

--- src/components/PlanItem.js
+++ src/components/PlanItem.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 function PlanItem({ hour, title, review }) {
-  const addHourPad = num => {
-    return num.toString().padStart(2, '0');
+  const addHourPad = (num) => {
+    return num.toString().padStart(2, "0");
   };
 
   return (
     <ItemWrapper>
       <Hour>{`${addHourPad(hour)} - ${addHourPad(hour + 1)}`}</Hour>
-      <Item>{title}</Item>
-      <Item>{review}</Item>
+      <Title>{title}</Title>
+      <Review>{review}</Review>
     </ItemWrapper>
   );
 }
@@ -21,19 +21,27 @@ export default PlanItem;
 const ItemWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  border-bottom: dotted 1px #333333;
+  border-bottom: dotted 1px #36837b;
   padding: 0px 2px;
 `;
 
 const Hour = styled.div`
   text-align: center;
   flex: 2;
+  padding: 5px 0px;
 `;
 
-const Item = styled.div`
-  border-left: red 1px solid;
+const Title = styled.div`
+  border-left: red double;
+  // border-style: double;
   text-align: start;
   flex: 4;
-  padding: 0px 5px;
-  `;
-  
+  padding: 5px;
+`;
+
+const Review = styled.div`
+  border-left: grey 1px solid;
+  text-align: start;
+  flex: 4;
+  padding: 5px;
+`;
--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import styled from '@emotion/styled'
+import React from "react";
+import styled from "@emotion/styled";
 
-import PlanItem from './PlanItem';
-
+import PlanItem from "./PlanItem";
 
 export default function Plans() {
   const hours = [...Array(24).keys()];
+  const currentHour = new Date().getHours();
 
   return (
-    <PlanWrapper>
-      {hours.map(hour => (
-        <PlanItem key={hour} hour={hour} title={'title'} review={'review'} />
+    <LegalPad>
+      {hours.map((hour) => (
+        <PlanItem key={hour} hour={hour} title={"title"} review={"review"} />
       ))}
-    </PlanWrapper>
+    </LegalPad>
   );
 }
 
-
-const PlanWrapper = styled.div`
+const LegalPad = styled.div`
+  background: #faf4a1;
   border: #222222 solid 2px;
   padding: 0;
-  margin: 5px;
-`
+`;
